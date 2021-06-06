c888b71af201dd819021b3677788c272f28db99c
Norox95 <janick.metzger@stud.hshl.de>
2021-06-06 17:17:34 +0200

    MergeTry

--- Pharmacy/src/application/Main.java
+++ Pharmacy/src/application/Main.java
@@ -1,16 +1,23 @@
 package application;
 
+import java.io.FileNotFoundException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import products.MedicineSearch;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 
 public class Main extends Application {
+	static Stage stg;
+
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws Exception {
+		this.stg = primaryStage;
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root, 400, 400);
+			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("Login.fxml"));
+			Scene scene = new Scene(root, 650, 500);
 			scene.getStylesheets().add(getClass().getResource("/gui/application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -18,67 +25,12 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-
-	/**
-	 * public static void main(String[] args) { launch(args);
-	 * 
-	 * }
-	 * 
-	 * File Login = new File ("User.txt"); try { Scanner read = new Scanner(Login);
-	 * String line;
-	 * 
-	 * while (read.hasNextLine() && !isBlank(line = read.nextLine())) { String[]
-	 * accountData = line.split("/"); String user = accountData[0]; String password
-	 * = accountData[1]; System.out.println(user+ ", " +password); } }
-	 * 
-	 * private static boolean isBlank(String s) { return s == null || s.isEmpty(); }
-	 */
-
-	/*
-	 * public static boolean login(String user, String pass) {
-	 * 
-	 * 
-	 * //create users.txt file File file = new File("users.txt");
-	 * 
-	 * FileReader fr;
-	 * 
-	 * boolean Login = false;
-	 * 
-	 * 
-	 * try {
-	 * 
-	 * fr = new FileReader(file); BufferedReader br = new BufferedReader(fr); String
-	 * line;
-	 * 
-	 * while((line = br.readLine()) !=null) { String[] split = line.split(", ");
-	 * String username; String password;
-	 * 
-	 * username = split[0]; password = split[1]; } } finally{
-	 * 
-	 * } return Login;
-	 * 
-	 * }
-	 * 
-	 * public static ArrayList<User> getUsersList() { ArrayList<User> userList = new
-	 * ArrayList<>(); if (username.equals(user) && password.equals(pass)) { Login =
-	 * true; break; } return userList; =======
-	 * 
-	 * public static void main(String[] args) throws FileNotFoundException {
-	 * launch(args);
-	 * 
-	 * MedicineSearch.showProductAssortment();
-	 * 
-	 * 
-	 * 
-	 *  }
-	 * 
-	 * public static void addUser() {
-	 * 
-	 * }
-	 * 
-	 * private void addUserToFile(String username, String password) { if(!Login) {
-	 * try { System.out.println("Anmeldung nicht möglich"); }catch (IOException e) {
-	 * 
-	 * } } }
-	 */
+	public static void main(String[] args) throws FileNotFoundException {
+		  launch(args);
+		 
+		 MedicineSearch.showProductAssortment();
+		 
+		 
+		 
+		  }
 }
